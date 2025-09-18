eed47a70ad0ca09fe3f970b88c16ba8bdf1fe4a7
Wiches09 <109953465+Wiches09@users.noreply.github.com>
2025-09-18 19:49:30 +0700

    minor fix

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -5,8 +5,8 @@ try:
     if (age >= 0):
         print("You are", age, "years old.")
         print("In 10 years, you'll be", age+10, "years old")
-        print("In 10 years, you'll be", age+20, "years old")
-        print("In 10 years, you'll be", age+30, "years old")
+        print("In 20 years, you'll be", age+20, "years old")
+        print("In 30 years, you'll be", age+30, "years old")
 
 except:
     print("this is not a number")
